5fed1faf30fcd20ffba8bb7e6e44282ce00b23b6
l2jong <gkwkdlawoh@naver.com>
2021-02-02 22:12:56 +0900

    mqtt

---
+++ src/main/java/lemonfree/springboot/mqtt/MqttApplication.java
@@ -0,0 +1,12 @@
+package lemonfree.springboot.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttApplication.class, args);
+	}
+}
